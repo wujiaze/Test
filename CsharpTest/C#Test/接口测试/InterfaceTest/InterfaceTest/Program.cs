17dfc6f1fca95da4ba83d2c8ae606632ef259d3c
wujiaze <1051277749@qq.com>
2018-10-11 11:23:42 +0800

    2018/10/11 周四

--- CsharpTest/C#Test/接口测试/InterfaceTest/InterfaceTest/Program.cs
+++ CsharpTest/C#Test/接口测试/InterfaceTest/InterfaceTest/Program.cs
@@ -14,10 +14,12 @@ namespace InterfaceTest
         }
         static void Main(string[] args)
         {
+            
             Program p1 = new Program();
             p1.Set(new Class1());
             p1.Set(new Class2());
             p1.test.print("111111111");   // 这步说明，接口对象只执行最后赋值的类的方法
+
 
             // 下面两步印证了上面的结论
             Program p2 = new Program();
