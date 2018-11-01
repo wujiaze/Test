832a2d3f14275540599def46ca4cdc85c02c360f
wujiaze <1051277749@qq.com>
2018-11-01 22:57:58 +0800

    2018/11/01 周四

---
+++ CsharpTest/C#Test/数组引用的测试/ArrayTest/ArrayTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[] { 100 };
+            ChangeByte(bytes);
+            Console.WriteLine(bytes[0]);
+            Console.Read();
+        }
+
+        private static void ChangeByte(byte[] bytes)
+        {
+            bytes[0] = 10;
+        }
+    }
+}
