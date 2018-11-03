748a7c954c871441b77e795af0327fc260e1d0de
wujiaze <1051277749@qq.com>
2018-11-03 18:43:54 +0800

    2018/11/03 周六

---
+++ UnityTest/UnityTest/Assets/41_测试Mono的回调函数/CallBackTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ *  SetActive(true),多次和一次是一样的，就是激活，然后就会执行 OnEnable ，就是加了个判断，所以不会多次调用 OnEnable
+ */
+public class CallBackTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+    private void OnEnable()
+    {
+        print("OnEnable");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ UnityTest/UnityTest/Assets/41_测试Mono的回调函数/ControllerTEst.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerTEst : MonoBehaviour
+{
+
+    public GameObject obj;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.A))
+	    {
+	        obj.SetActive(true);
+        }
+	    if (Input.GetKeyDown(KeyCode.B))
+	    {
+	        obj.SetActive(false);
+	    }
+    }
+}
