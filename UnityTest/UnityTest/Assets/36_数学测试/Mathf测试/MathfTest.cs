e45be5fb35a2558a838801b62b0e2091f7f135ac
wujiaze <1051277749@qq.com>
2018-11-08 20:23:00 +0800

    2018/11/08 周四

--- UnityTest/UnityTest/Assets/36_数学测试/Mathf测试/MathfTest.cs
+++ UnityTest/UnityTest/Assets/36_数学测试/Mathf测试/MathfTest.cs
@@ -1,141 +1,146 @@
 ﻿/*
- *      Mathf 类解析
+ *      Mathf 结构体解析
  *
- *      从角度转到弧度的常数
- *          Mathf.Deg2Rad
- *      从弧度转到角度的常数
- *          Mathf.Rad2Deg
+ *      一、分类
+ *          1、基础类
+ *              1.1、静态常数
+ *                      Mathf.Deg2Rad                               从角度转到弧度的常数
+ *                      Mathf.Rad2Deg                               从弧度转到角度的常数
+ *                      Mathf.Epsilon                               区别于 0 的最小浮点数
+ *                      Mathf.Infinity                              正无穷大
+ *                      Mathf.NegativeInfinity                      负无穷大
+ *                      Mathf.PI                                    圆周率 π 的值
+ *              1.2、三角函数(参数弧度角)
+ *                      Mathf.Sin                                   弧度角的正弦值
+ *                      Mathf.Cos                                   弧度角的余弦值
+ *                      Mathf.Tan                                   弧度角的正切值
+ *                      Mathf.Asin                                  通过反正弦求弧度角
+ *                      Mathf.Acos                                  通过反余弦求弧度角
+ *                      Mathf.Atan                                  通过反正切求弧度角
+ *                      Mathf.Atan2                                 通过反正切2求弧度角(推荐，因为这个可以求90度，即x=0，也能求值)
+ *              1.3、数值转换
+ *                      Mathf.Abs                                   绝对值
+ *                      Mathf.Ceil                                  向上取整(返回 float)
+ *                      Mathf.CeilToInt                             向上取整(返回 int)
+ *                      Mathf.Floor                                 向下取整(返回 float)
+ *                      Mathf.FloorToInt                            向下取整(返回 int)
+ *                      Mathf.Clamp                                 限制取值范围
+ *                      Mathf.Clamp01                               限制取值范围，范围[0,1]
+ *                      Mathf.Exp                                   自然指数 e 的次方数
+ *                      Mathf.Log                                   取对数 f：值  p: 对数的底(默认为 自然指数 e)
+ *                      Mathf.Log10                                 取对数 f: 值     对数的底为10
+ *                      Mathf.Pow                                   取指数 f: 底  p: 指数
+ *                      Mathf.Sqrt                                  开方数(小于0，返回NaN)
+ *                      Mathf.Round                                 返回最近的数，当0.5结尾时，返回最近的两个数之中的偶数
+ *                      Mathf.RoundToInt                            返回值转换为Int
+ *              1.4、数值处理
+ *                      Mathf.Max                                   最大值(两个数，一个数组)
+ *                      Mathf.Min                                   最小值(两个数，一个数组)
+ *                      Mathf.Sign（f）                              返回值 1：正 0：负。特别的 0为返回为1，表示正
+ *              1.5、曲线变换
+ *                      Mathf.LerpUnclamped(a,b,t)                  返回值 = a + (b - a) * t  ,就是一个普通的线性变换，可以看成一个函数：f(x) = ax + b
+ *          2、复合类
+ *              2.1、数值处理
+ *                      Mathf.Approximately                         比较两个浮点值，是否相近(精度在E-40)，推荐使用（因为在上亿次计算中 1.0 == 10.0 / 10.0，也有可能出错）
  *
- *      区别于 0 的最小浮点数
- *          Mathf.Epsilon
+ *                      Mathf.DeltaAngle                            获取两个角的最小夹角，范围在(-180,180]
+ *                                                                  原理：先 Repeat(target-current,360),并且大于180时，减去180
  *
- *      正无穷大
- *          Mathf.Infinity
- *      负无穷大
- *          Mathf.NegativeInfinity
+ *                      Mathf.InverseLerp(from,to,value)            反插值,计算两个值之间的Lerp参数
+ *                                                                  原理 ： 插值 = value-from/ to-from，然后再使用 Mathf.Clamp01 进行限制
  *
- *      圆周率 π 的值
- *          Mathf.PI
+ *              2.2、曲线变换
+ *                      Mathf.Repeat(t,length)                      周期性重复或者说周期性限制  每当 t 大于等于 length ，就返回 t % length
  *
- *      绝对值
- *          Mathf.Abs()
+ *                      Mathf.PingPong                              周期性重复(曲线类似乒乓)
+ *                                                                  原理 ： 内部就是 Mathf.Repeat ，外加一些处理
  *
- *      浮点数比较
- *          Mathf.Approximately    // 浮点数的比较其实都是不精准的，可以采用近似来比较，这个方法的精度大概在 E-45，是非常高的精确度了
+ *                      Mathf.Lerp(start,end,t)                     线性插值
+ *                                                                  原理： 返回值 = (end - start)* Clamp01(t) + start,就是一种普通的线性变换 ,并且内部对 t 做了限制范围是[0,1]
+ *                                                                  建议使用方式：start 和 end 在Update中都是不变的，改变的是 t，可以累加，也可以减少
+ *                                                                  用法:详见Update
+ *                      Mathf.LerpAngle(a,b,t)                      角度线性插值
+ *                                                                  原理：先使用 Mathf.DeltaAngle(b-a,360) ，然后使用 Lerp 的原理：返回值 = a + Mathf.DeltaAngle * Clamp01(t),,并且内部对 t 做了限制范围是[0,1] 
+ *                                                                  建议使用方式：a 和 b 在Update中都是不变的，改变的是 t，可以累加，也可以减少
  *
- *      向上取整(返回 float)
- *          Mathf.Ceil
- *      向上取整(返回 int)
- *          Mathf.CeilToInt
+ *                      Mathf.MoveTowards(current,target,maxDelta)  移向
+ *                                                                  原理 ：返回值 = current + maxDelta，当 返回值 > target 时，返回 target。特点：当 maxDelta 为 负数时，则反向运动，且没有限制
+ *                                                                  将这个方法作为线性运动：见Update中(还有其他的做法，不过推荐这一种，因为这是官方的解释)
+ *                                                                  用法:详见Update
+ *                      Mathf.MoveTowardsAngle                      角度移向
+ *                                                                  原理:先 计算出真正的 target = Mathf.DeltaAngle + current，然后用 Mathf.MoveTowards        
+ *                                                                  用作线性变换时，与 MoveTowards 一致
  *
- *      限制取值范围
- *      Mathf.Clamp
- *      Mathf.Clamp01  特殊：min=0 max=1
+ *                      Mathf.SmoothDamp                            平滑阻尼（随着时间的推移逐渐改变一个值到目标值）区别与线性变换Lerp
+ *                                                                  用法:详见Update
+ *                      Mathf.SmoothDampAngle                       角度平滑阻尼
+ *                                                                  原理：先确定真正的 target = Mathf.DeltaAngle + current 然后使用 Mathf.SmoothDamp
+ *                                                                  
+ *                      Mathf.SmoothStep                            平滑插值:逐渐加速，后减速
+ *                                                                  用法:详见Update
+ *          3、未开源类
+ *              3.1、数值处理
+ *                  Mathf.ClosestPowerOfTwo(value)                  返回距离value最近的2的次方数
+ *                                                                  特殊1：value 自身是2的次方数，则返回 value  特殊2：若是在中间，则向上取值
  *
- *      返回距离value最近的2的次方数，若 value 自身是2的次方数，则返回 value
- *      Mathf.ClosestPowerOfTwo(value)   // 若是在中间，则向上取值
- *      判断是否是 2 的次方数
- *      Mathf.IsPowerOfTwo
- *      返回 大于 value 的下一个 2的次方数，当value自身就是 2 的次方数时，就直接返回value
- *       Mathf.NextPowerOfTwo(value)
- *  
- *      将开尔文温度转化为Color(温度的范围 1000~4000)
- *          Mathf.CorrelatedColorTemperatureToRGB
+ *                  Mathf.IsPowerOfTwo                              判断是否是 2 的次方数
  *
- *      获取两个角的最小夹角，范围在(-180,180]
- *          Mathf.DeltaAngle
- *
- *      自然指数 e 的n次方数
- *          Mathf.Exp
- *
- *      向下取整(返回 float)
- *          Mathf.Floor
- *      向下取整(返回 int)
- *          Mathf.FloorToInt
- *
- *      取对数
- *      Mathf.Log   f：值  p: 对数的底(默认为 自然指数 e)
- *      Mathf.Log10  f:值  对数的底为10
- *
- *      最大值
- *      Mathf.Max  在一个float 或 int  的数组中，获取最大值
- *      最小值
- *      Mathf.Min  在一个float 或 int  的数组中，获取最小值
- *
- *      三角函数：
- *      Mathf.Acos()
- *      Mathf.Asin()
- *      Mathf.Atan()
- *      Mathf.Atan2()
- *      Mathf.Cos()
- *
- *      周期性重复
- *      Mathf.Repeat(t,length)      每当 t 大于等于 length ，就返回 t % length
- *
- *      柏林噪声
- *      Mathf.PerlinNoise   柏林噪声：一种伪随机的函数，每一个坐标点都可以获取确定的值，只需要取值区域不同，就可以得到类似随机的值
- *
- *      插值类
- *      线性插值
- *      Mathf.Lerp          计算方式 (end-start)*t+start 
- *                          正确用法是：start 和 end 在Update中都是不变的，改变的一致是 t ,t 的在内部的限制范围是[0,1]
- *      Mathf.LerpUnclamped 与Lerp一致，不同点：对t没有限制
- *
- *      角度线性插值(原理和Lerp一样，只是内部对180和大于180的数值做了处理)
- *      Mathf.LerpAngle     首先计算角度： 把需要变化的角度都转换为(-180,180]
- *                          然后就是 Lerp 的计算方法，t的有效范围是[0,1]
- *
- *      反插值 : 计算两个值之间的Lerp参数
- *      Mathf.InverseLerp   计算方式 （value - from）/（to - from）,返回值[0,1]
- *
- *      Move 类
- *      Mathf.MoveTowards  本质上也是一种线性插值，优点是：不会超过 target
- *                         如何使用：见Update中(还有其他的做法，不过推荐这一种，因为这是官方的解释)
- *                         特点：当 maxDelta 为 负数时，则反向运动，且没有限制
- *      Mathf.MoveTowardsAngle  源码为:先通过 Mathf.DeltaAngle 计算出 真正的target，然后用 Mathf.MoveTowards
- *
+ *                  Mathf.NextPowerOfTwo(value)                     返回 大于 value 的下一个 2的次方数
+ *                                                                  特殊：当value自身就是 2 的次方数时，就直接返回value
+ *              3.2、其他
+ *                  Mathf.CorrelatedColorTemperatureToRGB           将开尔文温度转化为Color(温度的范围 1000~4000)
+ *                  Mathf.PerlinNoise                               林噪声：一种伪随机的函数，每一个坐标点都可以获取确定的值，只需要取值区域不同，就可以得到类似随机的值
+ *                  Mathf.GammaToLinearSpace                        todo 以下两者主要用来做颜色转换，用在shader中，以后有空再来看
+ *                  Mathf.LinearToGammaSpace  
  */
 using UnityEngine;
 
 public class MathfTest : MonoBehaviour
 {
+    public Transform cubeLerp;
+    public Transform cubeMoveToward;
+    public Transform cubeSmoothDamp;
+    public Transform cubeSmoothStep;
 
-    // Use this for initialization
-    void Start()
-    {
-        print(Mathf.ClosestPowerOfTwo(8));
-    }
 
+    private float speed = 0.5f; //说明是 2秒完成
     private float timer = 0;
-    private float start = 5;
+    private float start = -5;
     private float target = 10;
+    private float currentVelocity = 0;
+    private float timersmoothStep = 0;
     void Update()
     {
-        timer += 0.5f * Time.deltaTime;                // 2 可以是看作 1/2 秒完成线性插值，因为 Time.deltaTime 累加在1s时间中就是1，即完成线性插值
-                                                       // 现在 累加的速度 变成 2倍，那么完成时间就减少一倍，是1秒的 1/2 倍，即0.5秒
-        float temp = Mathf.Lerp(5, 10, timer);      // 线性插值的意思：每一段时间间隔，移动的距离应该是一样的
-        //print("temp " + temp);
-        //float angle = Mathf.LerpAngle(0, 359, timer);
-        //print("angle " + angle);
+        // Lerp 用法 
+        timer += Time.deltaTime * speed;                                                            // 1/speed 秒完成线性插值，因为 Time.deltaTime 累加在1s时间中就是1，即完成线性插值
+                                                                                                    // 现在 累加的速度 变成 speed 倍，那么完成时间是1秒的 1/speed 倍
+        float temp = Mathf.Lerp(-5, 10, timer);                                                     // 线性插值的意思：每一段时间间隔，移动的距离应该是一样的
+        cubeLerp.position = new Vector3(temp, cubeLerp.position.y, 0);
+        print("temp " + temp);
+        // MoveTowards 用法
+        start = Mathf.MoveTowards(start, target, speed * Time.deltaTime * (10 + 5));                // 根据源码可知： 返回值 = start + maxDelta ，当 start+maxDelta > end,返回值 = target
+                                                                                                    // 那么为了符合线性变化：maxDelta = 每一帧变化的值，总变化 =  target-start
+                                                                                                    // 每一帧变化的值 = 总变化 * Time.deltaTime  那么就表示，1秒达到target，所以还可以增加 speed
+                                                                                                    // 最后  maxDelta = (target-start)*Time.deltaTime*speed   ,那么 时间 = 1/speed
+                                                                                                    // 最后 返回值需要 赋给 start ，这样就可以每帧变化相同的值，线性变化达到目标
+        cubeMoveToward.position = new Vector3(start, cubeMoveToward.position.y, 0);
+        print("start " + start);
+        // SmoothDamp 平滑阻尼
+        float x = Mathf.SmoothDamp(cubeSmoothDamp.position.x, 10, ref currentVelocity, 1 / speed);   // 根据源码可知:使用方法 current 应为不断改变的值
+                                                                                                     //                     target   为目标值
+                                                                                                     //                     currentVelocity 为当前速度，必须为全局变量，一开始设置为0速度即可(当然也可以设置为其他)     
+                                                                                                     //                     smoothTime  阻尼运动的大致时间    
+                                                                                                     //                     另外两个参数：maxSpeed deltaTime 一般默认的即可
+        cubeSmoothDamp.position = new Vector3(x, cubeSmoothDamp.position.y, 0);
+        print("x " + x);
 
-
-        start = Mathf.MoveTowards(start, target, 2 * Time.deltaTime * (10 - 5));
-        //print("move " + start);                     // 根据源码可知： 返回值 = start + maxDelta ，当 start+maxDelta > end,返回值 = target
-        // 那么为了符合线性变化：maxDelta = 每一帧变化的值，总变化 =  target-start
-        // 每一帧变化的值 = 总变化 * Time.deltaTime  那么就表示，1秒达到target，所以还可以增加 speed
-        // 最后  maxDelta = (target-start)*Time.deltaTime*speed   ,那么 时间 = 1/speed
-        // 最后 返回值需要 赋给 start ，这样就可以每帧变化相同的值，线性变化达到目标
-        //Mathf.MoveTowardsAngle()
-
-        print(Mathf.PerlinNoise(10, 10));
-
-      
+        // SmoothStep 平滑插值
+        timersmoothStep += speed * Time.deltaTime;
+        float dex = Mathf.SmoothStep(-5, 10, timersmoothStep);                                        // 平滑插值：使用方式与 Lerp 类似 ，确定 from 和 to ,然后确定smoothTime大致时间
+        cubeSmoothStep.position = new Vector3(dex, cubeSmoothStep.position.y, 0);
+        print("dex " + dex);
     }
 
-    private void Awake()
-    {
-
-    }
 
 
 }
---
+++ UnityTest/UnityTest/Assets/36_数学测试/Vector3测试/Vector3Test.cs
@@ -0,0 +1,82 @@
+﻿/*
+ *      Vector3 结构体解析
+ *      基础
+ *      向量点乘 (a1,b1,c1,d1,....) * (a2,b2,c2,d2,....) = a1*a2 + b1*b2 + c1*c2 + d1*d2....
+ *      三维向量叉乘 (a1,b1,c1) x (a2,b2,c2) = (b1*c2-b2*c1,a1*c2-a2*c1,a1*b2-a2*b1)
+ *      一、分类
+ *          1、基础类
+ *              1.1、静态常数
+ *                      Vector2.up                  (0, 1)
+ *                      Vector2.down                (0,-1)
+ *                      Vector2.left                (-1,0)
+ *                      Vector2.right               ( 1,0)
+ *                      Vector2.one                 ( 1,1)
+ *                      Vector2.zero                ( 0,0)
+ *                      Vector2.positiveInfinity    (正无穷，正无穷)
+ *                      Vector2.negativeInfinity    (负无穷，负无穷)
+ *              1.2、数值转换
+ *                      magnitude               向量长度(一般用来精确计算长度)
+ *                      sqrMagnitude            向量长度平方(一般用来比较向量的长短，而不需要精确长度)
+ *                      normalized              标准化，向量自身不改变,返回向量长度为1，方向不变
+ *                      Normalize               标准化，向量自身改变,         长度为1，方向不变
+ *              1.3、数值处理
+ *                      Vector2.Max             返回一个新的 Vector2 ,x 为两个向量.x中较大的那个，y 为两个向量.y中较大的那个
+ *                      Vector2.Min             返回一个新的 Vector2 ,x 为两个向量.x中较小的那个，y 为两个向量.y中较小的那个
+ *                      Vector2.ClampMagnitude  限制向量长度，当向量小于等于限制长度时，返回原向量，当向量大于限制长度时，返回限制长度的向量
+ *                      Vector2.Distance        返回两向量之间的距离      
+ *                      Vector2.Dot             向量点乘  a1*a2+b1*b2
+ *                                              源码：a.x*b.x + a.y*b.y   
+ *                                              用途:1、根据几何意义，可以用Acos求夹角，可以用来判断一个向量对另一个向量是否可见
+ *                                                   2、当没有零向量时，返回值为0 ，则为垂直
+ *                                                   3、计算一个向量在另一个向量上的投影大小
+ *                                                   4、返回值大于0，结合其他(比如长度) ，判断在前方
+ *                                                      返回值小于0，结合其他(比如长度) ，判断在后方
+ *                                                      返回值等于0，结合其他(比如长度) ，判断在左边或右边(可再结合三维的叉乘)
+ *                      Vector2.Scale           缩放向量，原向量.x * sclae.x   原向量.y * sclae.y
+ *              1.4、曲线变换
+ *                      Vector2.LerpUnclamped   线性变换（对 t 没有限制）
+ *                                              原理：对两向量的x,y 分别使用 Mathf.LerpUnclamped
+ *                                                    也可以整体理解，将 向量看作一个整体，进行线性插值
+ *Vector3.Cross
+ *
+ *
+ *          2、复合类
+ *              2.1、
+ *                      Vector2.Angle           两向量的夹角，内部采用了 Vector2.Dot 点乘来计算角度
+ *                                              由于同一个cosA ,可能有正负两个值，这里直接取了正值
+ *                                              角度的范围[0,180]
+ *                      Vector2.SignedAngle     两向量的夹角，先通过 Vector2.Angle 求值，在通过叉乘的Z轴的正负，来判断角的正负
+ *                                              又由于 Mathf.sign: 0 为正，所以角度的取值范围(-180,180]
+ *                      Vector2.Reflect         求 direction 相对于 法向量 normal 的 反射向量
+ *                                              准确的条件: 法向量必须是 标准化向量
+ *              2.2、曲线变换
+ *                      Vector2.Lerp            线性插值，类似于Mathf.Lerp,详见用法
+ *                      Vector2.MoveTowards     移向,这个移向即可以做线性运动，也可以做非线性运动
+ *                                              但是，这个函数设计出来就是为了线性运动，所以建议用作线性运动，非线性运动 使用   Vector2.SmoothDamp
+ *                                              使用方法见Update
+ *                      Vector2.SmoothDamp      平滑阻尼（随着时间的推移逐渐改变一个值到目标值）区别与线性变换Lerp
+ *
+ *
+ * Vector3.Project
+ * Vector3.ProjectOnPlane
+ *          3.未开源类
+ *      Vector3.OrthoNormalize
+ * Vector3.RotateTowards
+ * Vector3.Slerp
+ * Vector3.SlerpUnclamped
+ */
+using UnityEngine;
+
+public class Vector3Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
